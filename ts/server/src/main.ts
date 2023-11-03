8f149892839a48945399dbabb5c44f7558be4457
sRRRs <srrrs4510@gmail.com>
2023-11-03 18:05:58 +0900

    [wasm]deno server

--- ts/server/src/main.ts
+++ ts/server/src/main.ts
@@ -1,5 +1,13 @@
 import { instantiate } from "../lib/rs_lib.generated.js";
+import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 
 const { add } = await instantiate();
 
-console.log(add(1, 2));
+const total = add(1, 2);
+console.log(total);
+serve((req: Request) => new Response(JSON.stringify(total), {
+    status: 200,
+    headers: {
+        "content-type": "application/json"
+    }
+}));
